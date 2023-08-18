6663e7d6997b3c94a1a7cd47981e58521ef8be8c
ipsluizeduardo <ipsluizeduardo@gmail.com>
2023-08-18 18:56:42 -0300

    Map
    Signed-off-by: ipsluizeduardo <ipsluizeduardo@gmail.com>

---
+++ src/main/java/map/Ordenacao/AgendaEventos.java
@@ -0,0 +1,52 @@
+package main.java.map.Ordenacao;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+
+public class AgendaEventos {
+    //atributo
+    private Map<LocalDate, Evento> eventoMap;
+
+    public AgendaEventos() {
+        this.eventoMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao) {
+        eventoMap.put(data, new Evento(nome, atracao));
+    }
+
+    public void exibirAgenda() {
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventoMap);
+        System.out.println(eventosTreeMap);
+    }
+
+    public void obterProximoEvento() {
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        LocalDate dataAtual = LocalDate.now();
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventoMap);
+        for(Map.Entry<LocalDate, Evento> entry : eventoMap.entrySet()) {
+            if(entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)) {
+                proximaData = entry.getKey();
+                proximoEvento = entry.getValue();
+                System.out.println("O proximo evento: " + proximoEvento + " acontecera na data " + proximaData);
+                break;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Evento 1", "Atração 1");
+        agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Evento 2", "Atração 2");
+        agendaEventos.adicionarEvento(LocalDate.of(2000, Month.JANUARY, 10), "Evento 3", "Atração 3");
+        agendaEventos.adicionarEvento(LocalDate.of(2023, Month.JULY, 14), "Evento 5", "Atração 5");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, Month.SEPTEMBER, 20), "Evento 6", "Atração 6");
+
+        agendaEventos.exibirAgenda();
+
+        agendaEventos.obterProximoEvento();
+    }
+}
